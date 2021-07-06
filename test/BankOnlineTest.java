e8ea50a838d8edc4132153c88c99ba9fa0c3dc02
Inga <in264659@yandex.ru>
2021-07-06 19:36:16 +0300

    Исправленный вариант

--- test/BankOnlineTest.java
+++ test/BankOnlineTest.java
@@ -6,18 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BankOnlineTest {
     BankOnline bankOnline = new BankOnline();
+    int test;
 
     @Test
-    void isBlocked() throws IOException {
-        int test = bankOnline.isBlocked("1111111111111111");
+    void isBlocked1() throws IOException {
+        test = bankOnline.isBlocked("1111111111111111");
         assertEquals(0, test);
+    }
 
+    @Test
+    void isBlocked2() throws IOException {
         test = bankOnline.isBlocked("1211111111111111");
         assertEquals(3, test);
+    }
 
+    @Test
+    void isBlocked3() throws IOException {
         test = bankOnline.isBlocked("2222222222222222");
         assertEquals(0, test);
+    }
 
+    @Test
+    void isBlocked4() throws IOException {
         test = bankOnline.isBlocked("1111222222222222");
         assertEquals(3, test);
     }
